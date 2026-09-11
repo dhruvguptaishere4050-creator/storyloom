f3fef870616363bcd6553e81aec9eb64ac5f6432
DHRUV GUPTA <dhruvguptaishere4050@gmail.com>
2026-09-12 02:20:54 +0530

    Add responsive Storyloom landing site

---
+++ site/sw.js
@@ -0,0 +1,8 @@
+const CACHE = 'storyloom-landing-v1';
+const FILES = ['./', './index.html', './manifest.webmanifest', './favicon.svg'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
